1ce6635bea60d2a820f4360b51bee836c164c36e
chaaerim <cofla411@ewhain.net>
2023-01-27 21:48:26 +0900

    feat : add provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,30 @@
 import type { AppProps } from 'next/app';
+import type { ComponentProps } from 'react';
+import { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import GlobalErrorFallback from '@/components/GlobalErrorFallback';
+import QueryClientProvider from '@/components/QueryClientProvider';
+import QueryErrorBoundary from '@/components/QueryErrorBoundary';
+import RecoilDebugObserver from '@/components/RecolDebugObserver';
+
+interface PageProps {
+  dehydratedState: ComponentProps<typeof QueryClientProvider>['dehydratedState'];
 }
+
+function MyApp({ Component, pageProps }: AppProps<PageProps>) {
+  return (
+    <RecoilRoot>
+      <QueryClientProvider dehydratedState={pageProps.dehydratedState}>
+        <QueryErrorBoundary ErrorFallback={GlobalErrorFallback}>
+          <RecoilDebugObserver />
+          <Suspense fallback={<>Global Suspense</>}>
+            <Component {...pageProps} />
+          </Suspense>
+        </QueryErrorBoundary>
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
