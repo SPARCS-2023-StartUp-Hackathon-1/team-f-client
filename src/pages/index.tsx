a51d01efa4d5914a0ab7055adcff06b1c16d6666
chaaerim <cofla411@ewhain.net>
2023-01-28 01:55:50 +0900

    refactor : button 내부에서 hover, disable 처리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/common/Button';
+
 export default function Home() {
-  return <>Hello Next</>;
+  return (
+    <div className="flex-col text-center align-middle">
+      <header className="mt-[75px] text-h2 text-gray-600">Service Name</header>
+      <div className="text-b1 mt-[12px] text-gray-600">AI 꼬리질문으로 준비하는 면접</div>
+      <div className="mt-[24px] w-[328px] h-[328px] bg-gray-100 mb-[24px]"></div>
+      <Button buttonType="primary" isDisabled={true} buttonText="시작하기" />
+    </div>
+  );
 }
