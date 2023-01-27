97133c3879d80b63075ed9b9f7dbd657411afc20
chaerim kim <89721027+chaaerim@users.noreply.github.com>
2023-01-27 22:39:52 +0900

    dev branch와 동기화  (#6)
    * Feat/icon (#5)
    
    * feat : add custom scroll
    
    * feat : install svgr
    
    * test : test push
    
    * fix : fix import error

---
+++ src/components/common/Icon/icons.ts
@@ -0,0 +1,19 @@
+import { default as check } from './icons/check.svg';
+import { default as downArrow } from './icons/downArrow.svg';
+import { default as leftArrow } from './icons/leftArrow.svg';
+import { default as rightArrow } from './icons/rightArrow.svg';
+import { default as setting } from './icons/setting.svg';
+import { default as toGoHome } from './icons/toGoHome.svg';
+import { default as upArrow } from './icons/upArrow.svg';
+
+export const ICONS = {
+  check: check,
+  downArrow: downArrow,
+  leftArrow: leftArrow,
+  rightArrow: rightArrow,
+  setting: setting,
+  toGoHome: toGoHome,
+  upArrow: upArrow,
+};
+
+export type IconName = keyof typeof ICONS;
