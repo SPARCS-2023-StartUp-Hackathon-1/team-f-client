c44629b95e4bda5b8d264c4e5f5ee8fa112d9de3
DongHyun Lee <donghyuni98@gmail.com>
2023-01-28 12:57:33 +0900

    Feat/onboarding UI (#18)
    * style: 컴포넌트 스타일 변경 사항 추가
    
    * refactor: gnb 컴포넌트 변경
    
    * feat(layout): layout 컴포넌트 추가
    
    * feat: option 컴포넌트 스켈레톤 추가
    
    * feat: 온보딩 화면 GNB 적용

--- src/components/common/Select/Option.tsx
+++ src/components/common/Select/Option.tsx
@@ -19,11 +19,15 @@ export const Option = ({ id, name }: OptionProps) => {
     <li
       id={`${id}`}
       ref={selectRef}
-      className={`flex justify-between items-center h-[5.6rem] rounded-[0.8rem] p-[1.6rem] ${OPTION_STYLE[selected]}`}
+      className={`flex justify-between items-center h-[5.6rem] rounded-[0.8rem] p-[1.6rem] mt-[1rem] ${OPTION_STYLE[selected]}`}
       onClick={handleSelect}
     >
       <p className="text-b1">{name}</p>
-      {selected === 'selected' && <Icon width={18} height={14} iconName="check" />}
+      {selected === 'selected' ? (
+        <Icon width={18} height={14} iconName="check" />
+      ) : (
+        <div className="w-[1.8rem] h-[1.4rem]"></div>
+      )}
     </li>
   );
 };
