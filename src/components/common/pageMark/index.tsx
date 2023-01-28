2b5e17c420c6e8fb3285c7e7659c924294f6e0d3
chaerim kim <89721027+chaaerim@users.noreply.github.com>
2023-01-28 10:53:02 +0900

    feat : pageMark 컴포넌트 구현 (#14)

---
+++ src/components/common/pageMark/index.tsx
@@ -0,0 +1,19 @@
+,export interface PageMarkProps {
+    pageNumber: number;
+    isActive: boolean;
+  }
+  
+  export const PageMark = ({ pageNumber, isActive }: PageMarkProps) => {
+    return (
+      <div
+        className={`flex w-[36px] h-[36px] text-b2 items-center justify-center ${
+          isActive
+            ? 'text-primary-default border-2 border-primary-default rounded-[50%] bg-gray-0'
+            : 'text-gray-300 bg-gray-0'
+        }`}
+      >
+        {pageNumber}
+      </div>
+    );
+  };
+  
