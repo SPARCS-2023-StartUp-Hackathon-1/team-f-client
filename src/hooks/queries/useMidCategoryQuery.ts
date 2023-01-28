eb2f000496ceff7f5f5522e0af49aac78d612ea2
chaaerim <cofla411@ewhain.net>
2023-01-29 02:43:25 +0900

    fix : fix lint error

--- src/hooks/queries/useMidCategoryQuery.ts
+++ src/hooks/queries/useMidCategoryQuery.ts
@@ -5,6 +5,7 @@ export const useMidCategoryQuery = ({ id }: { id: string }) => {
   const midCategoryQuery = useQuery({
     queryKey: ['midCategories'],
     queryFn: () => getMidCategories({ id }),
+    enabled: !!id,
   });
   return midCategoryQuery;
 };
