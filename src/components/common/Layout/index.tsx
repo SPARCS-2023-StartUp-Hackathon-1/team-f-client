d8c7ebe0bd63710f638f67eaa5fb400a83202f8d
chaaerim <cofla411@ewhain.net>
2023-01-28 19:28:23 +0900

    feat : path에 따라 backgroundcolor설정

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,7 +1,17 @@
-import { PropsWithChildren } from 'react';
+import { useRouter } from 'next/router';
+import { PropsWithChildren, useState } from 'react';
 
 const Layout = ({ children }: PropsWithChildren) => {
-  return <main className="bg-gray-50 px-[1.6rem] h-full">{children}</main>;
+  const router = useRouter();
+  return (
+    <>
+      {router.pathname === '/' || router.pathname === '/onboarding/[mainCategoryId]' ? (
+        <main className="bg-gray-0 px-[1.6rem] h-full">{children}</main>
+      ) : (
+        <main className="bg-gray-50 px-[1.6rem] h-full">{children}</main>
+      )}
+    </>
+  );
 };
 
 export default Layout;
